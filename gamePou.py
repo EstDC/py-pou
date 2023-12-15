86135ebe91b203213327c8fcb3aefb223f728a27
edc <147658033+EstDC@users.noreply.github.com>
2023-12-15 10:37:17 +0100

    Add files via upload

--- gamePou.py
+++ gamePou.py
@@ -9,13 +9,14 @@ import random
 
 class Pou:
 
-    def __init__(self, name, age, hunger, energy, happiness, health, status):
+    def __init__(self, name, age, hunger, energy, happiness, health, status, clean):
         self.name = name
         self.age = age 
         self.hunger= hunger 
         self.energy= energy 
         self.happiness= happiness
         self.health= health 
+        self.clean = clean
         self.alive = True
         
         
@@ -23,10 +24,20 @@ class Pou:
         self.hunger -= 1
         self.energy -= 2
         self.health += 1
+        self.clean -=1
+        if (self.happiness >=10):
+            print("I don't want to play anymore.")
         if(self.energy <=5):
             self.happiness -= 1
         else:
             self.happiness += 5
+        if any(value <= 2 for value in (self.energy, self.hunger, self.health, self.clean, self.happiness)):
+            print("\n\nʕ ´•̥̥̥ ᴥ•̥̥̥`ʔ\n")
+        elif any(value <= 5 for value in (self.energy, self.hunger, self.health, self.clean, self.happiness)):
+            print("\n\nʕ •ᴥ•ʔ\n")
+        else:
+            print("\n\nʕ´ᵔ ᴥᵔ`ʔ\n")
+        
         print(self.status)
 
     def eat(self):
@@ -34,6 +45,17 @@ class Pou:
         self.energy -= 1
         self.happiness +=1
         self.health += 2
+        self.clean -=3
+        if (self.hunger >=10):
+            print("I don't want to eat anymore.")
+        
+        if any(value <= 2 for value in (self.energy, self.hunger, self.health, self.clean, self.happiness)):
+            print("\n\nʕ ´•̥̥̥ ᴥ•̥̥̥`ʔ\n")
+        elif any(value <= 5 for value in (self.energy, self.hunger, self.health, self.clean, self.happiness)):
+            print("\n\nʕ •ᴥ•ʔ\n")
+        else:
+            print("\n\nʕ´ᵔ ᴥᵔ`ʔ\n")
+        
         print(self.status)
     
     def sleep(self):
@@ -41,6 +63,38 @@ class Pou:
         self.energy += 5
         self.happiness +=1
         self.health += 2
+        self.clean -=1
+        if (self.energy >=10):
+            print("I don't want to sleep anymore.")
+        
+        if any(value <= 2 for value in (self.energy, self.hunger, self.health, self.clean, self.happiness)):
+            print("\n\nʕ ´•̥̥̥ ᴥ•̥̥̥`ʔ\n")
+        elif any(value <= 5 for value in (self.energy, self.hunger, self.health, self.clean, self.happiness)):
+            print("\n\nʕ •ᴥ•ʔ\n")
+        else:
+            print("\n\nʕ´ᵔ ᴥᵔ`ʔ\n")
+        
+        print(self.status)
+
+    def cleaning(self):
+        self.hunger -= 1
+        self.energy -= 1
+        self.happiness +=3
+        self.health += 1
+        self.clean +=5
+        if (self.clean >=10):
+            print("I'm clean enough.")
+        
+        if any(value <= 2 for value in (self.energy, self.hunger, self.health, self.clean, self.happiness)):
+            print("\n\nʕ ´•̥̥̥ ᴥ•̥̥̥`ʔ")
+
+        elif any(value <= 5 for value in (self.energy, self.hunger, self.health, self.clean, self.happiness)):
+            print("\n\nʕ •ᴥ•ʔ")
+
+        else:
+            print("\n\nʕ´ᵔ ᴥᵔ`ʔ")
+
+        
         print(self.status)
 
         
@@ -51,29 +105,40 @@ class Pou:
         print("Energy: ", self.energy)
         print("Happiness: ", self.happiness)
         print("Helth: ", self.health)
+        print("\n")
+        print("\n")
+        if self.energy == 0 and self.hunger == 0 and self.health == 0:{
+            print( "\n X ᴥ Xʔ   R.I.P. \n")
+        }
 
 
-coco = Pou("coco",random.randint(1,3), random.randint(0,10), random.randint(0,10), random.randint(0,10),random.randint(0,10),random.randint(0,10))
+coco = Pou("coco",random.randint(1,3), random.randint(0,10), random.randint(0,10), random.randint(0,10),random.randint(0,10),random.randint(0,10), random.randint(0,10))
 print()
 
 
 while True:
     print("What do you want to do?")
-    print("Eat")
-    print("Sleep")
-    print("Play")
-    print("Show status")
-    print("Exit")
+    print("=======================")
+    print("1. Eat")
+    print("2. Sleep")
+    print("3. Play")
+    print("4. Cleaning")
+    print("5. Show status")
+    print("6. Exit")
     option = input()
-    if option == "Eat":
+    if option == "1":
         coco.eat()
-    elif option == "Sleep":
+    elif option == "2":
         coco.sleep()
-    elif option == "Pay":
+    elif option == "3":
         coco.play()
-    elif option == "Show status":
+    elif option == "4":
+        coco.cleaning()
+    elif option == "5":
         coco.status()
-    elif option == "Exit":
+    elif option == "6":
         break
     else:
         print("What do you want to do?")
+        print("\n")
+        print("\n")
